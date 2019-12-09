740070bb102a9e844b455ac1f61a4ed50db16833
Anna Grace <agrace@exeter.edu>
2019-12-09 15:37:22 -0500

    Fixed Scene Switching Problem
    Got the challenge Storyboard Working, Hallaluyah

--- 30DayChallengeTracker/Views/Challenge Page View/ChallengeViewController.swift
+++ 30DayChallengeTracker/Views/Challenge Page View/ChallengeViewController.swift
@@ -12,7 +12,6 @@ class ChallengeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
     
